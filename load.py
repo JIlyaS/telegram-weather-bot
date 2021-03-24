15df90a0e9e9fee67ea5203fcde84f93d39ea0de
JIlyaS <ilkolmakov@yandex.ru>
2021-03-24 12:49:48 +0500

    add redis for bot

--- load.py
+++ load.py
@@ -1,26 +1,43 @@
+import os
 import json
+import redis
 import consts
+
+redis_url = os.environ.get('REDIS_URL')
+
+init_data = {
+    "weather_api": "https://api.weatherbit.io/v2.0",
+    "states": {},
+    "cities": [],
+    "history_weather": {},
+    "city_weather": {},
+    consts.MAIN_STATE: {},
+    consts.CITY_STATE: {},
+    consts.WEATHER_DATE_STATE: {},
+    consts.SETTINGS_STATE: {}
+}
 
 data = {}
 
-try:
-    data = json.load(open('db/data.json', 'r', encoding='utf-8'))
-except FileNotFoundError:
-    data = {
-        "token": "1673485604:AAHHwK3mfal0ITHT9V50OAMoSBnG2ER4da0",
-        "weather_key": "9f254c735ad3417791385f3af65f8f75",
-        "weather_api": "https://api.weatherbit.io/v2.0",
-        "states": {},
-        "cities": [],
-        "history_weather": {},
-        "city_weather": {},
-        consts.MAIN_STATE: {},
-        consts.CITY_STATE: {},
-        consts.WEATHER_DATE_STATE: {},
-        consts.SETTINGS_STATE: {}
-    }
+if redis_url is None:
+    try:
+        data = json.load(open('db/data.json', 'r', encoding='utf-8'))
+    except FileNotFoundError:
+        data = init_data
+else:
+    redis_db = redis.from_url(REDIS_URL)
+    raw_data = redis_db.get('data')
+    if raw_data is None:
+        data = init_data
+    else:
+        data = json.loads(raw_data)
 
 
 def change_data(key, user_id, value):
     data[key][user_id] = value
-    json.dump(data, open('db/data.json', 'w', encoding='utf-8'), indent=2, ensure_ascii=False)
+    if redis_url is None:
+        json.dump(data, open('db/data.json', 'w', encoding='utf-8'), indent=2, ensure_ascii=False)
+    else:
+        redis_db = redis.from_url(REDIS_URL)
+        redis_db.set('data', json.dumps(data))
+        
